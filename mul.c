2a091b05fd91f5e5e9efdce6da975bf562156391
22bcad09anusha <anushahegdeamma1981@gmail.com>
2024-02-26 11:24:13 +0530

    first file

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, product;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    product = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, product);
+    return 0;
